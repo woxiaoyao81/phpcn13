85b66c564ffd8b778ed5deee120d4bdd2117e044
woxiaoyao <53898168+woxiaoyao81@users.noreply.github.com>
2020-12-07 08:22:12 +0800

    Update demo.php

--- 12020304/demo.php
+++ 12020304/demo.php
@@ -1,33 +1,48 @@
 <?php
-
-function isAssoc(array $arr)
+$isAssoc=function (array $arr)
 {
     if (is_array($arr)) {
         $key = array_keys($arr);
         return $key === array_keys($key) ? false : true;
     }
     return null;
-}
+};
 
+class Check{
+    static function checkArray(Closure $isAssoc,array $arr){
+        return $isAssoc($arr);
+    }
+}
+$arr=[1,2,3,4,5,6];
+var_dump(Check::checkArray($isAssoc,$arr));
+
+// function isAssoc(array $arr)
+// {
+//     if (is_array($arr)) {
+//         $key = array_keys($arr);
+//         return $key === array_keys($key) ? false : true;
+//     }
+//     return null;
+// }
 // $arr=[1,2,3,4,5,6];
-$arr = ['asd', 2, 3, 4];
+// $arr = ['asd', 2, 3, 4];
 // $arr = array('name' => 'xiaoyao','pwd'=>'3234234');
 // var_dump(array_keys($arr));
 // var_dump(array_keys(array_keys($arr)));
 // var_dump(isAssoc($arr));
 
 
-$data = array('name' => 'xiaoyao','pwd'=>'3234234');
-$where=array('name'=>'woxiaoyao');
-$sql = "update user set ";
-$str = "";
-foreach (array_keys($data) as $column) {
-    $str .= $column . " = ?,";
-}
-$sql .= substr($str, 0, -1);
-$str = "";
-foreach (array_keys($where) as $item) {
-    $str .= " " . $item . " = ? and";
-}
-$sql .= " where " . substr($str, 0, -4);
-echo $sql;
+// $data = array('name' => 'xiaoyao','pwd'=>'3234234');
+// $where=array('name'=>'woxiaoyao');
+// $sql = "update user set ";
+// $str = "";
+// foreach (array_keys($data) as $column) {
+//     $str .= $column . " = ?,";
+// }
+// $sql .= substr($str, 0, -1);
+// $str = "";
+// foreach (array_keys($where) as $item) {
+//     $str .= " " . $item . " = ? and";
+// }
+// $sql .= " where " . substr($str, 0, -4);
+// echo $sql;
